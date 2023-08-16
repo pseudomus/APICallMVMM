3871fce13216415ad50b2294f841206a72dbc97e
Luca Lacerda <lucagabriel520@gmail.com>
2023-08-16 14:26:15 -0300

    Initial Commit

---
+++ mvvmAPI/mvvmAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  mvvmAPIApp.swift
+//  mvvmAPI
+//
+//  Created by Luca Lacerda on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct mvvmAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
